0ad324af58c604d0ea7378045c07059db414e914
Jorge Trejo <jorgeetrejoo@gmail.com>
2025-02-27 17:51:47 -0600

    Se arreglo el responsive de las páginas de autenticación

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,8 +2,8 @@ import {ReactNode} from "react";
 
 export default function AuthLayout({children}: {children: ReactNode}) {
   return (
-    <div className="relative min-h-screen overflow-hidden flex items-center justify-center bg-gradient-to-b from-black via-black to-[#1f1f1f] p-5">
-      <div className="w-full">{children}</div>
+    <div className="relative h-[100dvh] overflow-auto flex items-center justify-center bg-gradient-to-b from-black via-black to-[#1f1f1f] lg:p-10">
+      <div className="w-full h-full">{children}</div>
     </div>
   );
 }
