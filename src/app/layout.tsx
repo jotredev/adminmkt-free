9cc2ff47fa81d401a20135df5f703a2be524412c
Jorge Trejo <jorgeetrejoo@gmail.com>
2025-04-23 18:34:11 -0600

    Se agrego el script de Attributa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,37 @@
-import type {Metadata} from "next";
-import {Urbanist} from "next/font/google";
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from "@/providers/theme-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 import NextTopLoader from "nextjs-toploader";
-import {Analytics} from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const font = Urbanist({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
   title: "AdminMKT - FREE",
   description:
-    "AdminMKT - Plantilla de administración para markeplace de código abierto."
+    "AdminMKT - Plantilla de administración para markeplace de código abierto.",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://attributa-server.onrender.com/t.js"
+          data-domain="cm9um5wza000lc82ixpdftie1"
+          defer
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={font.className}>
         <ThemeProvider
           attribute="class"
